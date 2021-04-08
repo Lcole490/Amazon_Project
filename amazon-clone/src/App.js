173eb1f293c1bfea1d2c0aab242f0da863326744
Lcole.engr <lcole.engr@gmail.com>
2021-04-08 13:29:08 -0400

    connected App.js to cartItems collection in firebase db

--- amazon-clone/src/App.js
+++ amazon-clone/src/App.js
@@ -4,9 +4,36 @@ import Header from './Components/Header';
 import Cart from './Components/Cart'
 import Home from './Components/Home'
 import {BrowserRouter as Router, Switch, Route, Link} from 'react-router-dom'
+import {useState, useEffect} from 'react'
+import { db } from './firebase'
 
 
 function App() {
+
+
+  const [cartItems, setCartItems] = useState([]);
+
+  const getCartItems = () => {
+    db.collection('cartItems').onSnapshot((snapshot) => {
+      const tempItems = snapshot.docs.map((doc) =>({
+        id: doc.id, 
+        product: doc.data()
+      }));
+
+      setCartItems(tempItems)
+    })
+
+  }
+
+
+  useEffect(()=>{
+
+    getCartItems();
+
+  }, [])   //    UseEffect runs the function upon loading of page,
+
+  console.log(cartItems)
+
   return (
     <div className="App">
     <Router>
